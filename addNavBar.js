bc9b39e6e4e7b769bdc4464969ecff51181081c4
kylethayer <kyle.thayer@gmail.com>
2019-04-14 14:56:29 -0700

    Add navbar to most views

--- addNavBar.js
+++ addNavBar.js
@@ -1,8 +1,16 @@
 $( document ).ready(function() {
+	let rootPath = "";
+	let visPath = "";
+	if(window.location.pathname.indexOf("index.html") > 0){
+		visPath = "vis/";
+	} else{
+		rootPath = "../";
+	}
+	
 	let navBarStr = "";
 	
 	navBarStr += '<nav class="navbar navbar-expand-lg navbar-light bg-light">';
-	navBarStr += '  <a class="navbar-brand" href="index.html">ManyLanguagesManyColors</a>';
+	navBarStr += '  <a class="navbar-brand" href="'+rootPath+'index.html">ManyLanguagesManyColors</a>';
 	navBarStr += '  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">';
 	navBarStr += '    <span class="navbar-toggler-icon"></span>';
 	navBarStr += '  </button>';
@@ -10,7 +18,7 @@ $( document ).ready(function() {
 	navBarStr += '  <div class="collapse navbar-collapse" id="navbarSupportedContent">';
  	navBarStr += '   <ul class="navbar-nav mr-auto">';
 	navBarStr += '      <li class="nav-item active">';
- 	navBarStr += '       <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>';
+ 	navBarStr += '       <a class="nav-link" href="'+rootPath+'index.html">Home <span class="sr-only">(current)</span></a>';
 	navBarStr += '      </li>';
 	navBarStr += '      <li class="nav-item">';
 	navBarStr += '        <a class="nav-link" href="http://labinthewild.org/studies/color_perception/">Take the Survey</a>';
@@ -20,11 +28,11 @@ $( document ).ready(function() {
 	navBarStr += '         Visualizations';
 	navBarStr += '        </a>';
 	navBarStr += '        <div class="dropdown-menu" aria-labelledby="navbarDropdown">';
-	navBarStr += '          <a class="dropdown-item" href="vis/color_compare.html">Color Comparisons</a>';
-	navBarStr += '          <a class="dropdown-item" href="vis/stacked-spectrum.html">Hue Color Comparisons (more languages)</a>';
-	navBarStr += '          <a class="dropdown-item" href="vis/full_color_maps.html)">Color Map</a>';
-	navBarStr += '          <a class="dropdown-item" href="vis/translation.html)">Korean-English Translation Comparison</a>';
-	navBarStr += '          <a class="dropdown-item" href="vis/viridis.html)">Korean-English Viridis Color Spectrum</a>';
+	navBarStr += '          <a class="dropdown-item" href="'+visPath+'color_compare.html">Color Comparisons</a>';
+	navBarStr += '          <a class="dropdown-item" href="'+visPath+'stacked-spectrum.html">Hue Color Comparisons (more languages)</a>';
+	navBarStr += '          <a class="dropdown-item" href="'+visPath+'full_color_maps.html">Color Map</a>';
+	navBarStr += '          <a class="dropdown-item" href="'+visPath+'translation.html">Korean-English Translation Comparison</a>';
+	navBarStr += '          <a class="dropdown-item" href="'+visPath+'viridis.html">Korean-English Viridis Color Spectrum</a>';
 	navBarStr += '        </div>';
 	navBarStr += '      </li>';
 	navBarStr += '      <li class="nav-item">';
