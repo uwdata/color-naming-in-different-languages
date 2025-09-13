2053cbe86c251b9d91595d1465d2d498f041a7f7
Kyle <kmthayer@uw.edu>
2025-09-13 00:00:15 -0700

    rewrite getTranslation to use the new lab bin system

--- model/process/getTranslation.js
+++ model/process/getTranslation.js
@@ -1,8 +1,9 @@
 const fs = require('fs'),
   d3 = require('d3'),
-  labBinner = require('./labBinner.js');
+  labBinHelper = require('./labBinHelper.js');
+
+const lab_bins = JSON.parse(fs.readFileSync("../lab_bins.json"))
 let flatData = JSON.parse(fs.readFileSync("../full_color_names.json"));
-const BIN_SIZE = 10, TOP_N = 100;
 let colorNamesAbrv = {
 	"English": "en",
 	'Korean': "ko",
@@ -12,7 +13,12 @@ let colorNamesAbrv = {
 	"French": "fr",
 	"Portuguese": "pt",
 	"Spanish": "es",
-	"Swedish": "sv"
+	"Swedish": "sv",	
+  "Russian": "ru",
+	"Dutch": "nl",
+	"Polish": "pl",
+	"Finnish": "fi",
+	"Romanian": "ro"
 };
 
 // 1.
@@ -20,7 +26,7 @@ if (!fs.existsSync("temp/")){
     fs.mkdirSync("temp/");
 }
 convertToMatrices();
-fs.writeFileSync("temp/distanceMatrix.json", JSON.stringify(getDistanceMetrix()));
+fs.writeFileSync("temp/distanceMatrix.json", JSON.stringify(getDistanceMatrix()));
 console.log("Please run getEMD.py on python 2 to transition_loss.json.");
 
 
@@ -62,9 +68,9 @@ function convertToMatrices(){
     g_lang.values = g_lang.values.sort((a,b) => d3.sum(b.values, d => d.cnt) - d3.sum(a.values, d => d.cnt));
     
     g_lang.values.forEach(g_term => {
-      let labCnt = labBinner.createLABBins(BIN_SIZE);
-      let labPct = labBinner.createLABBins(BIN_SIZE);
-      let labPtc = labBinner.createLABBins(BIN_SIZE);
+      let labCnt = labBinHelper.createLABNumBins(lab_bins);
+      let labPct = labBinHelper.createLABNumBins(lab_bins);
+      let labPtc = labBinHelper.createLABNumBins(lab_bins);
       g_term.values.forEach(d => {
         labCnt[d.binL][d.binA][d.binB] = d.cnt;
         labPct[d.binL][d.binA][d.binB] = d.pCT;
@@ -75,44 +81,41 @@ function convertToMatrices(){
       terms.push({
         "term": g_term.key,
         "lang": g_lang.key,
-        "labCnt": convertTo1D(convertTo1D(labCnt)),
-        "labPct": convertTo1D(convertTo1D(labPct)),
-        "labPtc": convertTo1D(convertTo1D(labPtc))
+        "labCnt": labBinHelper.labBinsToArray(labCnt),
+        "labPct": labBinHelper.labBinsToArray(labPct),
+        "labPtc": labBinHelper.labBinsToArray(labPtc)
       });
     });
     fs.writeFileSync(`temp/fullColorNames_${colorNamesAbrv[g_lang.key.split("(")[0].trim()]}.json`, JSON.stringify(terms, null, 2));
   });
 
 }
-function getDistanceMetrix(){
-  let [nL, nA, nB] = labBinner.createLABBinN(BIN_SIZE);
-  let MSize = nL * nA * nB;
+function getDistanceMatrix(){
+  const labBinArr = labBinHelper.labBinsToArray(lab_bins)
+  MSize = labBinArr.length
   let distM = new Array(MSize).fill(0);
   distM = distM.map(d => {
     return new Array(MSize).fill(0);
   });
 
   for (let i = 0; i < MSize; i++) {
-    let l_i = Math.floor(i / (nA * nB));
-    let a_i = Math.floor((i % (nA * nB)) / nB);
-    let b_i = (i % (nA * nB)) % nB;
+    const [l_i, a_i, b_i] = [labBinArr[i].l_center, labBinArr[i].a_center, labBinArr[i].b_center]
 
 
     for (let j = 0; j < MSize; j++) {
-      let l_j = Math.floor(j / (nA * nB));
-      let a_j = Math.floor((j % (nA * nB)) / nB);
-      let b_j = (j % (nA * nB)) % nB;
+      const [l_j, a_j, b_j] = [labBinArr[j].l_center, labBinArr[j].a_center, labBinArr[j].b_center]
+
       distM[i][j] = Math.sqrt(Math.pow(l_i - l_j,2) + Math.pow(a_i - a_j,2) + Math.pow(b_i - b_j,2));
     }
   }
   return distM;
 }
 
-function convertTo1D(arr){
-  return arr.reduce((acc, curr) => {
-    acc = acc.concat(curr);
-    return acc;
-  }, []);
-}
+// function convertTo1D(arr){
+//   return arr.reduce((acc, curr) => {
+//     acc = acc.concat(curr);
+//     return acc;
+//   }, []);
+// }
 
 
