e8cfc3175de688192d26a0c805208f7151c9cb98
Kyle <kmthayer@uw.edu>
2025-09-23 12:28:23 -0700

    Calculate avg color as part of basic_full_color_info now that we we can use "hue color ratio" instead of binning

--- model/process/getBasicFullColorInfo.js
+++ model/process/getBasicFullColorInfo.js
@@ -28,6 +28,8 @@ let colorNamesAbrv = {
 	"Romanian": "ro"
 };
 
+const HUE_COLOR_RATIO = JSON.parse(fs.readFileSync("./lab_hue_color_ratio.json")).hueColorRatio;
+
 function getLangAbv(lang){
   const langKey = Object.keys(colorNamesAbrv).find(val => lang.includes(val))
   const abv = colorNamesAbrv[langKey]
@@ -60,6 +62,13 @@ csv().fromFile(FILE_I)
         term.commonName = d3.groups(term.values,t => t.standardized_entered_name)
           .map(a => {return {key: a[0], values: a[1]}})
           .sort((a,b) => -a.values.length + b.values.length)[0].key;
+        const avgLab = getAverageLABColor(term.values)
+        const avgRgb = avgLab.rgb()
+        //const lab 
+        term.avgColorRGBCode = `rgb(${Math.round(avgRgb.r)},${Math.round(avgRgb.g)},${Math.round(avgRgb.b)})`
+        term.avgL = avgLab.l
+        term.avgA = avgLab.a
+        term.avgB = avgLab.b
       })
 
       g.terms = g.terms.filter(g_term => g_term.numFullNames >= MIN_FULL_COLOR_NAMES);
@@ -82,6 +91,10 @@ csv().fromFile(FILE_I)
         lang_abv: getLangAbv(term.lang),
         commonName: term.commonName,
         simplifiedName: term.simplifiedName,
+        avgColorRGBCode: term.avgColorRGBCode,
+        avgL: term.avgL,
+        avgA: term.avgA,
+        avgB: term.avgB,
         numFullNames: term.numFullNames,
         numLineNames: term.numLineNames,
       });
@@ -91,4 +104,47 @@ csv().fromFile(FILE_I)
 
   writer.end();
 });
+
+function getAverageLABColor(colorEntries){
+  const hueNames = colorEntries.filter(c => 
+    Math.max(c.r, c.g, c.b) == 255 && Math.min(c.r, c.g, c.b) == 0)
+    .map(c => d3.lab(d3.rgb(c.r,c.g,c.b)))
+  const nonHueNames = colorEntries.filter(c => 
+    ! (Math.max(c.r, c.g, c.b) == 255 && Math.min(c.r, c.g, c.b) == 0))
+    .map(c => d3.lab(d3.rgb(c.r,c.g,c.b)))
+
+  const numHueNames = hueNames.length
+  const numNonHueNames = nonHueNames.length
+  
+
+  const hueAvgLab = d3.lab(
+    hueNames.map(c => c.l).reduce((a, b) => a + Number(b), 0) / numHueNames,
+    hueNames.map(c => c.a).reduce((a, b) => a + Number(b), 0) / numHueNames,
+    hueNames.map(c => c.b).reduce((a, b) => a + Number(b), 0) / numHueNames,
+  )
+
+  const nonHueAvgLab = d3.lab(
+    nonHueNames.map(c => c.l).reduce((a, b) => a + Number(b), 0) / numNonHueNames,
+    nonHueNames.map(c => c.a).reduce((a, b) => a + Number(b), 0) / numNonHueNames,
+    nonHueNames.map(c => c.b).reduce((a, b) => a + Number(b), 0) / numNonHueNames,
+  )
+
+  // if only hue or non-hue names are present, then just return that one
+  if(numHueNames == 0){
+    return nonHueAvgLab
+  } 
+  if(numNonHueNames == 0){
+    return hueAvgLab
+  }
+
+  // if both hue and non-hue names are present, combine their averages together
+  // while correcting for the expected ratio of hue colors (in a random sample of LAB space)
+  const combinedAvgLab = d3.lab(
+    hueAvgLab.l * HUE_COLOR_RATIO + nonHueAvgLab.l * (1-HUE_COLOR_RATIO),
+    hueAvgLab.a * HUE_COLOR_RATIO + nonHueAvgLab.a * (1-HUE_COLOR_RATIO),
+    hueAvgLab.b * HUE_COLOR_RATIO + nonHueAvgLab.b * (1-HUE_COLOR_RATIO),
+  )
+
+  return combinedAvgLab
+}
 
