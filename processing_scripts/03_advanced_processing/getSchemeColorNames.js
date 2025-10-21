ce56279dc19ec5dacc8a573a299e133746ddd906
Kyle <kmthayer@uw.edu>
2025-10-21 14:58:14 -0700

    Add blurred bin option for scheme color names

--- processing_scripts/03_advanced_processing/getSchemeColorNames.js
+++ processing_scripts/03_advanced_processing/getSchemeColorNames.js
@@ -1,7 +1,18 @@
 const fs = require('fs'),
+  zlib = require('zlib'),
   d3 = require('d3'),
   labBinHelper = require('../utils/labBinHelper.js').getLabBins(10)
-let flatData = JSON.parse(fs.readFileSync("../../model/binned_full_colors/full_color_names_binned_10.json"));
+
+const NO_BLUR = "no-blur"
+const BLUR = "blur"
+
+const fullBinData = {}
+fullBinData[NO_BLUR] = JSON.parse(fs.readFileSync("../../model/binned_full_colors/full_color_names_binned_10.json"));
+fullBinData[BLUR] = JSON.parse(
+  zlib.unzipSync(
+    fs.readFileSync("../../model/binned_full_colors/full_color_names_binned_blur_10.json.gz")
+)); 
+
 const BIN_NUM = 10;
 const LANG_CODE = {
   'English (English)' : "en",
@@ -23,39 +34,43 @@ const schemes = [
   }
 ];
 
-let result = [];
-schemes.forEach(scheme => {
+for(const blur of [NO_BLUR, BLUR]){
+  let result = [];
+  schemes.forEach(scheme => {
 
-  Object.keys(LANG_CODE).forEach(lang => {
-    let data = [];
-    let previous = {};
-    for (var i = 0; i < (BIN_NUM + 1); i++) {
-      let lab = d3.lab(d3.color(scheme.fn(i/BIN_NUM)));
-      let [binL, binA, binB] = labBinHelper.bins_from_lab({l: lab.l, a: lab.a, b: lab.b})
-      let binData = flatData.filter(d => {
-        return d.lang === lang &&
-          d.binL === binL && d.binA === binA && d.binB === binB;
+    Object.keys(LANG_CODE).forEach(lang => {
+      let data = [];
+      for (var i = 0; i < (BIN_NUM + 1); i++) {
+        let lab = d3.lab(d3.color(scheme.fn(i/BIN_NUM)));
+        let [binL, binA, binB] = labBinHelper.bins_from_lab({l: lab.l, a: lab.a, b: lab.b})
+        let binData = fullBinData[blur].filter(d => {
+          return d.lang === lang &&
+            d.binL === binL && d.binA === binA && d.binB === binB;
+        });
+        data = data.concat(
+          binData.map(d => {
+            return {
+              "scheme": scheme.name,
+              "lang": lang,
+              "term": d.term,
+              "cnt": d.cnt,
+              "pTC": d.cnt / d3.sum(binData, d2 => d2.cnt),
+              "binNum": i,
+              "rgb": lab.rgb().toString()
+            };
+          })
+        );
+      }
+      data.forEach(d => {
+        d.pCT = d.cnt / d3.sum(data.filter(e => e.term === d.term), d => d.cnt);
       });
-      data = data.concat(
-        binData.map(d => {
-          return {
-            "scheme": scheme.name,
-            "lang": lang,
-            "term": d.term,
-            "cnt": d.cnt,
-            "pTC": d.cnt / d3.sum(binData, d2 => d2.cnt),
-            "binNum": i,
-            "rgb": lab.rgb().toString()
-          };
-        })
-      );
-    }
-    data.forEach(d => {
-      d.pCT = d.cnt / d3.sum(data.filter(e => e.term === d.term), d => d.cnt);
+      result = result.concat(data);
     });
-    result = result.concat(data);
   });
-});
 
-
-fs.writeFileSync("../../model/scheme_color_names.json", JSON.stringify(result, null, 2));
+  let blur_name = ""
+  if(blur == BLUR){
+    blur_name = "_blur"
+  }
+  fs.writeFileSync(`../../model/scheme_color_names${blur_name}.json`, JSON.stringify(result, null, 2));
+}
