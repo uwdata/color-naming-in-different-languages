2902daa05b3988ff5dd061285fbaef9c70308b86
Kyle <kmthayer@uw.edu>
2025-11-21 22:21:50 -0800

    improve home page and nav bar

--- addNavBar.js
+++ addNavBar.js
@@ -7,40 +7,42 @@ $( document ).ready(function() {
 		rootPath = "../";
 	}
 	
-	let navBarStr = "";
+	let navBarStr = `
 
-	navBarStr += '<nav class="navbar navbar-expand-lg navbar-light bg-light ps-5">';
-	navBarStr += '  <a class="navbar-brand" href="'+rootPath+'index.html">ManyLanguagesManyColors</a>';
-	navBarStr += '  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">';
-	navBarStr += '    <span class="navbar-toggler-icon"></span>';
-	navBarStr += '  </button>';
+	<nav class="navbar navbar-expand-lg navbar-light bg-light ps-5">
+	  <a class="navbar-brand" href="${rootPath}index.html">ManyLanguagesManyColors</a>
+	  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
 
-	navBarStr += '  <div class="collapse navbar-collapse" id="navbarSupportedContent">';
- 	navBarStr += '   <ul class="navbar-nav mr-auto">';
-	navBarStr += '      <li class="nav-item active">';
- 	navBarStr += '       <a class="nav-link" href="'+rootPath+'index.html">Home <span class="sr-only"></span></a>';
-	navBarStr += '      </li>';
-	navBarStr += '      <li class="nav-item">';
-	navBarStr += '        <a class="nav-link" href="http://labinthewild.org/studies/color_perception/" target="_blank">Take the Survey</a>';
-	navBarStr += '      </li>';
-	navBarStr += '      <li class="nav-item dropdown">';
-	navBarStr += '        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">';
-	navBarStr += '         Visualizations';
-	navBarStr += '        </a>';
-	navBarStr += '        <ul class="dropdown-menu">';
-	navBarStr += '          <li><a class="dropdown-item" href="'+visPath+'color_translator.html">Color Translator</a></li>';
-	navBarStr += '          <li><a class="dropdown-item" href="'+visPath+'stacked-spectrum.html">Hue Color Comparisons</a></li>';
-	navBarStr += '          <li><a class="dropdown-item" href="'+visPath+'full_color_maps.html">Full Color Maps</a></li>';
-	navBarStr += '          <li><a class="dropdown-item" href="'+visPath+'en-ko-translation-comparison.html">Korean-English Translation Comparison</a></li>';
-	navBarStr += '          <li><a class="dropdown-item" href="'+visPath+'viridis.html">Korean-English Viridis Color Spectrum</a></li>';
-	navBarStr += '        </ul>';
-	navBarStr += '      </li>';
-	navBarStr += '      <li class="nav-item">';
-	navBarStr += '        <a class="nav-link" href="https://github.com/uwdata/color-naming-in-different-languages/">Dataset</a>';
-	navBarStr += '      </li>';
-	navBarStr += '    </ul>';
-	navBarStr += '  </div>';
-	navBarStr += '</nav>';
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+ 	   <ul class="navbar-nav mr-auto">
+	      <li class="nav-item active">
+ 	       <a class="nav-link" href="${rootPath}index.html">Home <span class="sr-only"></span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="http://labinthewild.org/studies/color_perception/" target="_blank">Take the Survey</a>
+	      </li>
+	      <li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+	         Visualizations
+	        </a>
+	        <ul class="dropdown-menu">
+	          <li><a class="dropdown-item" href="${visPath}color_translator.html"><img src="imgs/translator-tiny.png" style="max-height:20px; max-width:50px" /> Color Translator</a></li>
+	          <li><a class="dropdown-item" href="${visPath}stacked-spectrum.html"><img src="imgs/hue-tiny.png" style="max-height:20px; max-width:50px" /> Hue Color Comparisons</a></li>
+	          <li><a class="dropdown-item" href="${visPath}full_color_maps.html"><img src="imgs/full-color-tiny.png" style="max-height:20px; max-width:50px" /> Full Color Maps</a></li>
+	          <li><a class="dropdown-item" href="${visPath}en-ko-translation-comparison.html">Korean-English Translation Comparison</a></li>
+	          <li><a class="dropdown-item" href="${visPath}viridis.html">Korean-English Viridis Color Spectrum</a></li>
+			  <li><a class="dropdown-item" href="${visPath}color-name-summaries.html">Color Name Summaries</a></li>
+	        </ul>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="https://github.com/uwdata/color-naming-in-different-languages/">Dataset</a>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
+	`
 	
 	$("body").prepend(navBarStr);
 	
