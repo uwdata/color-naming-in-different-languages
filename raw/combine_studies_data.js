e4ab664b5f1b80ccad86e4c938c2de9dfb25b104
Kyle <kmthayer@uw.edu>
2025-12-19 12:23:17 -0800

    Start loading in study v2 data

--- raw/combine_studies_data.js
+++ raw/combine_studies_data.js
@@ -3,24 +3,73 @@ import csv from 'csvtojson';
 import csvWriter from 'csv-write-stream'
 
 
-const STUDY_1_NAMES_I = "./study_v1/color_perception_table_color_names.csv"
+const STUDY_1_NAMES_I = "./study_v1/color_perception_table_color_names.csv" 
+
+const STUDY_2_I = "./study_v2/study_2_data.json"
+
 const NAMES_O = "./color_names.csv"
 
 // const STUDY_1_DEMOGRAPHICS_I = "./study_v1/color_perception_table_demographics.csv"
 // const DEMOGRAPHICS_O = "./demographics.csv"
 
 
-const color_names_writer = csvWriter();
+const v2_data = JSON.parse(fs.readFileSync(STUDY_2_I))
+
+
+const COLOR_NAME_STEPS = [1,3,5]
+///////////// COLOR NAMES /////////////////
+
+const color_names_writer = csvWriter({
+    headers: [
+        "participantId",
+        "lang0",
+        "name",
+        "colorSpace", "r", "g", "b",
+        "trialNum", "tileNum",
+        "rgbSet",
+        "background",
+        "locale",
+        "studyVersion"
+    ]});
 color_names_writer.pipe(fs.createWriteStream(NAMES_O));
 
 const v1_names = await csv().fromFile(STUDY_1_NAMES_I)
 for(const colorNameRow of v1_names){
+    delete colorNameRow.colorNameId
     delete colorNameRow.phaseNum
     delete colorNameRow.lab_l
     delete colorNameRow.lab_a
     delete colorNameRow.lab_b
     colorNameRow.colorSpace = "rgb"
+    colorNameRow.background = "white"
     color_names_writer.write(colorNameRow)
+}
+
+for(const participant of v2_data){
+    const color_name_sets = participant.study.data.color_name_set
+    if(color_name_sets[COLOR_NAME_STEPS[0]].color_names.length > 0){
+        for(const step of COLOR_NAME_STEPS){
+            const color_names = color_name_sets[step].color_names
+            for(const [name_i, name_info] of color_names.entries()){
+                const colorNameRow = {
+                    participantId:  participant.participant_id,
+                    lang0: color_name_sets[step].lang0,
+                    trialNum: color_name_sets[step].trialNum,
+                    tileNum: name_i,
+                    name: name_info.name,
+                    colorSpace: name_info.colorSpace,
+                    r: name_info.r,
+                    g: name_info.g,
+                    b: name_info.b,
+                    rgbSet: color_name_sets[step].rgbSet,
+                    studyVersion: color_name_sets[step].studyVersion,
+                    locale: color_name_sets[step].locale,
+                    background: color_name_sets[step].background,
+                }
+                color_names_writer.write(colorNameRow)
+            }
+        }
+    }
 }
 
 
---
+++ raw/study_v2/extract_relevant_data.js
@@ -0,0 +1,97 @@
+import fs from 'fs'
+import csv from 'csvtojson';
+import JSON5 from 'json5'
+
+const STUDY_2_DATA_I = "./download.csv"
+const NAMES_O = "./study_2_data.json"
+
+const FIELDS_CONVERT_JSON = ["color_names", "matches"]
+
+const FIELDS_TO_IGNORE = [
+    // timed study step tracking info
+    "break", "comments", "demographics", "informed_consent", "introduction", "results", "studyStep1", "studyStep2", "studyStep3", "studyStep4", "studyStep5",
+    
+    // other tracking info that feels too specific
+    ["litw", "initialize", "geoLoc", "city"],
+    ["litw", "initialize", "requester"],
+    ["litw", "initialize", "userAgent"],
+
+    // delete comments in case there is any personalized info
+    ["study", "comments"]
+]
+
+const study_2_data = await csv({checkType: true}).fromFile(STUDY_2_DATA_I)
+
+let participantInfo = []
+
+for(const participant_row of study_2_data){
+    const participant = {}
+    for(const [col, data] of Object.entries(participant_row)){
+        let currObject = participant
+        const dataLocation = col.split(":")
+        while(dataLocation.length > 1){
+            const field = dataLocation.shift()
+            if(!(field in currObject)){
+                currObject[field] = {}
+            }
+            currObject = currObject[field]
+        }
+        const lastLocation = dataLocation.shift()
+
+        let outputData = data
+        if(FIELDS_CONVERT_JSON.includes(lastLocation)){
+            if(data !== ""){
+                // Note: For some reason the data file was saved with single quotes, so use JSON5 to parse it
+                outputData = JSON5.parse(data)
+            }
+        }
+
+        // for date field, simplify to year for further anonymization
+        if(lastLocation == "date"){
+            outputData = (new Date(data)).getFullYear()
+        }
+
+        currObject[lastLocation] = outputData
+    }
+    participantInfo.push(participant)
+}
+
+
+// filter participants that have actual info
+participantInfo = participantInfo.filter(p => {
+    if(p.study.data.color_sorting_results[1].sortTiles[1].tilesOrder.length > 0){
+        return true
+    }
+    if(p.study.data.color_name_match_set[1].matches.length > 0){
+        return true
+    }
+    if(p.study.data.color_name_set[1].color_names.length > 0){
+        return true
+    }
+    return false
+})
+
+// Delete information we aren't interested in using
+for(const participant of participantInfo){
+    for(const field of FIELDS_TO_IGNORE){
+        if(typeof field == "string"){
+            delete participant[field]
+        } else { //assume array
+            // clone array
+            const fieldArray = [...field]
+            let currObject = participant
+            while(fieldArray.length > 1){
+                const subField = fieldArray.shift()
+                currObject = currObject[subField]
+            }
+            delete currObject[fieldArray.shift()]
+        }
+        
+    }
+}
+
+// console.log(JSON.stringify(participantInfo, null, 2))
+console.log("saving info from ", participantInfo.length, " participants")
+
+fs.writeFileSync(NAMES_O, JSON.stringify(participantInfo, null, 2));
+
