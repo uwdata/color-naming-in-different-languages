d32f4cff665561f7e2121789b4a08e535d481962
Kyle <kmthayer@uw.edu>
2025-12-22 20:22:12 -0800

    add in a little more Estonian data

---
+++ processing_scripts/01_data_cleaning/lang_rules/et_rules.js
@@ -0,0 +1,15 @@
+// Estonian (eesti, eesti keel)
+
+const excludeNames = [
+    // English names (at least one user entered first half in English, second in Estonian)
+    "pink", "purple", "green", "blue", "yellow", "red", "orange"
+];
+
+const nameReplacingRules = [
+
+];
+
+export default {
+    excludeNames: excludeNames,
+    nameReplacingRules: nameReplacingRules
+}
