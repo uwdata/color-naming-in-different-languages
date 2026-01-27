2048efeb0e02e6482ea5b3c4d1729dcd0dacf4a8
Kyle <kmthayer@uw.edu>
2026-01-27 01:03:22 -0800

    for color name summaries, get both hue and full to show up combined, along with SOM color sample

--- vis/js/color-name-summaries.js
+++ vis/js/color-name-summaries.js
@@ -14,18 +14,20 @@ let grid = undefined
 
 const hueColorNames = await d3.csv("../model/hue_colors_info.csv");
 const fullColorNames = await d3.csv("../model/full_colors_info.csv");
+const colorSampleSOMs = await (await fetch("../model/colorSOMPatches.json")).json();
+
 console.log(hueColorNames[0]);
 
 $("#selected_langs").change(e => { 
     updateTableData()
 })
 
-$("input:radio[name=color_set]").change(e => { 
-    updateColorSet()
+$("input:radio[name=rgb-set]").change(e => { 
+    updateRgbSet()
 })
 
 let currentDataset
-let currentDatasetColorSet
+let currentDatasetRgbSet
 let currentDatasetLangAbv
 $("#download_language_subset_button").click(e => {
     const csvContent = "data:text/csv;charset=utf-8," + d3.csvFormat(currentDataset)
@@ -35,35 +37,88 @@ $("#download_language_subset_button").click(e => {
     var encodedUri = encodeURI(csvContent);
     var link = document.createElement("a");
     link.setAttribute("href", encodedUri);
-    link.setAttribute("download", `${currentDatasetColorSet}_summaries_${currentDatasetLangAbv}.csv`);
+    link.setAttribute("download", `${currentDatasetRgbSet}_summaries_${currentDatasetLangAbv}.csv`);
     document.body.appendChild(link); // Required for FF
 
     link.click();
 })
 
-let allNamesByLang
+let allHueNamesByLang
+let allFullNamesByLang
+let allBothNamesByLang
 
 
-updateColorSet()
+updateRgbSet()
 
-function updateColorSet(){
-    const color_set_val = $('#full_colors').prop("checked") ? "full_colors" : "hue_colors"
-    let color_set = fullColorNames
-    currentDatasetColorSet = "full_color"
-    $("#source-data-link").attr("href", "https://github.com/uwdata/color-naming-in-different-languages/blob/master/model/full_colors_info.csv")
-    if(color_set_val == "hue_colors"){
-        color_set = hueColorNames
-        currentDatasetColorSet = "hue_color"
+function updateRgbSet(){
+    const rgbSet = $("input[name='rgb-set']:checked").val()
+
+    if(currentDatasetRgbSet == rgbSet){
+        return
+    }
+
+    currentDatasetRgbSet = rgbSet
+
+    let color_set
+    if(rgbSet == "both-hue-full"){
+        if(!allBothNamesByLang){
+            const fullNameSetByLang = Object.groupBy(fullColorNames, ({lang}) => lang)
+            const hueNameSetByLang = Object.groupBy(hueColorNames, ({lang}) => lang)
+            const allLangs = new Set([...Object.keys(fullNameSetByLang), ...Object.keys(hueNameSetByLang)])
+            allBothNamesByLang = {}
+            for(const lang of allLangs){
+                allBothNamesByLang[lang] = []
+                let allTerms = new Set()
+                if(lang in fullNameSetByLang){
+                    allTerms = new Set([...allTerms, ...fullNameSetByLang[lang].map(d=> d.simplifiedName)])
+                }
+                if(lang in hueNameSetByLang){
+                    allTerms = new Set([...allTerms, ...hueNameSetByLang[lang].map(d=> d.simplifiedName)])
+                }
+                for(const term of allTerms){
+                    const hueTermRow =  lang in hueNameSetByLang ? hueNameSetByLang[lang].find(d => d.simplifiedName == term) : undefined
+                    const fullTermRow =  lang in fullNameSetByLang ? fullNameSetByLang[lang].find(d => d.simplifiedName == term) : undefined
+                    const langAbv = fullTermRow ? fullTermRow.lang_abv : hueTermRow.lang_abv
+                    const somColorPatch = langAbv in colorSampleSOMs && term in colorSampleSOMs[langAbv] ? colorSampleSOMs[langAbv][term] : undefined
+                    allBothNamesByLang[lang].push({
+                        simplifiedName: term,
+                        commonName: hueTermRow ? hueTermRow.commonName : fullTermRow.commonName,
+                        avgColorRGBCode: fullTermRow ? fullTermRow.avgColorRGBCode : undefined,
+                        avgL: fullTermRow ? fullTermRow.avgL : undefined,
+                        avgA: fullTermRow ? fullTermRow.avgA : undefined,
+                        avgB: fullTermRow ? fullTermRow.avgB : undefined,
+                        avgHueColor: hueTermRow ? hueTermRow.avgHueColor : undefined,
+                        somColorPatch: somColorPatch
+                    })
+                }
+            }
+        }
+        color_set = allBothNamesByLang
+        $("#source-data-link").attr("href", "https://github.com/uwdata/color-naming-in-different-languages/blob/master/model/")
+    } else if(rgbSet == "full-data"){
+        if(!allFullNamesByLang){
+            allFullNamesByLang = Object.groupBy(fullColorNames, ({lang}) => lang)
+            for(const lang of Object.keys(allFullNamesByLang)){
+                for(const row of allFullNamesByLang[lang]){
+                    const langAbv = row.lang_abv
+                    const term = row.simplifiedName
+                    row.somColorPatch = langAbv in colorSampleSOMs && term in colorSampleSOMs[langAbv] ? colorSampleSOMs[langAbv][term] : undefined
+                }
+            }
+        }
+        color_set = allFullNamesByLang
+        $("#source-data-link").attr("href", "https://github.com/uwdata/color-naming-in-different-languages/blob/master/model/full_colors_info.csv")
+    } else { // hue
+        if(!allHueNamesByLang){
+            allHueNamesByLang = Object.groupBy(hueColorNames, ({lang}) => lang)
+        }
+        color_set = allHueNamesByLang
         $("#source-data-link").attr("href", "https://github.com/uwdata/color-naming-in-different-languages/blob/master/model/hue_colors_info.csv")
     }
 
-    allNamesByLang = Object.groupBy(color_set, ({lang}) => lang)
-
-    console.log(Object.keys(allNamesByLang))
-
     $("#selected_langs").empty()
     let selected_lang_temp = true
-    for(const lang of Object.keys(allNamesByLang)){
+    for(const lang of Object.keys(color_set).sort()){
         $("#selected_langs").append(new Option(lang, lang, true, selected_lang_temp))
         selected_lang_temp = false
     }
@@ -78,6 +133,18 @@ function updateTableData(){
     if(!selected_lang){
         return
     }
+
+    const rgbSet = $("input[name='rgb-set']:checked").val()
+
+    let allNamesByLang
+    if(rgbSet == "both-hue-full"){
+        allNamesByLang = allBothNamesByLang
+    } else if(rgbSet == "full-data"){
+        allNamesByLang = allFullNamesByLang
+    } else { // hue
+        allNamesByLang = allHueNamesByLang
+    }
+
 
     currentDatasetLangAbv = allNamesByLang[selected_lang][0].lang_abv
 
@@ -97,7 +164,8 @@ function updateTableData(){
                     },
                     formatter: (cell, row, col) => gridjs.html(`${escapeHTML(cell.commonName)}<br><span class="simplified-name">${escapeHTML(cell.simplifiedName)}</span>`)
                 },{
-                    name: "Color",
+                    id: "avgColor",
+                    name: gridjs.html('Avg Color<br><span class="simplified-name">full / hue</span>'),
                     data: (row) => row,
                     sort: {
                         compare: (a, b) => {
@@ -113,21 +181,44 @@ function updateTableData(){
                         }
                     },
                     formatter: (cell, row, col) => {
-                        const avgColor = "avgColorRGBCode" in cell ? cell.avgColorRGBCode : cell.avgHueColor
+                        //const avgColor = "avgColorRGBCode" in cell ? cell.avgColorRGBCode : cell.avgHueColor
                         return gridjs.html(`
                         <div
-                            style="height:20px; width: 20px; float:left; margin: 5px;
-                            background-color:${avgColor};" ></div>
-                        ${escapeHTML(avgColor)}`)
+                            style="height:30px; width: 30px; border-radius: 15px; float:left; margin: 5px;
+                            background-color:${cell.avgColorRGBCode ? cell.avgColorRGBCode : "rgba(0,0,0,0)"};" title="${escapeHTML(cell.avgColorRGBCode ? cell.avgColorRGBCode : "")}" >
+                        </div>
+                        <div
+                            style="height:30px; width: 30px; border-radius: 15px; float:left; margin: 5px;
+                            background-color:${cell.avgHueColor ? cell.avgHueColor : "rgba(0,0,0,0)"};" title="${escapeHTML(cell.avgHueColor ? cell.avgHueColor : "")}" >
+                        </div>
+                        `)
                     }
                 },
-                "Sample",
+                {
+                    id: "somColorPatch",
+                    name: "Sample",
+                    sort: false,
+                    formatter: (cell, row, col) => {
+                        //const avgColor = "avgColorRGBCode" in cell ? cell.avgColorRGBCode : cell.avgHueColor
+                        if(!cell){
+                            return ""
+                        }
+                        return gridjs.html(
+                            'colorNodes16' in cell ? generateColorGrid(cell.colorNodes16) :
+                            'colorNodes9' in cell ?  generateColorGrid(cell.colorNodes9) :
+                            generateColorGrid(cell.colorNodes4)
+                        )
+                    }
+                    
+                },
                 "Full Bins",
                 "Hue Bins",
                 "Name proportion / Commonality [phrasing]??"
                 ],
             sort: true,
-            search: true,
+            search:  {
+                selector: (cell, rowIndex, cellIndex) => cellIndex === 0 ? cell.commonName : cell
+            },
             pagination: true,
             data: nameData
         }).render(document.getElementById("data_table"));
@@ -137,6 +228,27 @@ function updateTableData(){
         }).forceRender();
     }
     
+}
+
+
+function generateColorGrid(nodes){
+    const totalGridPx = 40
+	let str = "";
+	for(let i = 0; i < nodes.length; i++){
+		for(let j = 0; j < nodes.length; j++){
+			let node = nodes[i][j];
+			str += "<div style='";
+			str += "width: "+totalGridPx/nodes.length+"px; height: "+totalGridPx/nodes.length+"px;float:left;"
+			str += "background-color:"+node.rgb;
+			str += "'><small>";
+			//str += Math.round(node.PCgN*100) + "%"
+			//str += Math.round(node.PCgN*100) + "%"
+			str += "</small></div>";
+		}
+		str += "<div style='clear:both'></div>";
+	}
+
+	return str;
 }
 
 
