9ef47aa53b785a589c858ab9f0b8321f0af08af0
Kyle <kmthayer@uw.edu>
2026-02-17 22:11:47 -0800

    update Persian color naming rules to combine more matching names (to take effect on future processing run)

--- processing_scripts/01_data_cleaning/lang_rules/fa_rules.js
+++ processing_scripts/01_data_cleaning/lang_rules/fa_rules.js
@@ -9,10 +9,24 @@ const excludeNames = [
 ];
 
 const nameReplacingRules = [
-    [/\u0653/g, ""] // "آ" -> "ا"
+    [/\u0653/g, ""], // "آ" -> "ا"
+
+    // simplify some characters
+    [/ي/g, "ی"],
+    [/ك/g, "ک"],
+
+    //arabic to farsi letters
+    [/ی/g, "ى"],
+
+
+    [/قهوهاى/g, "قهوه‌اى"],
+    [/قهوه اى/g, "قهوه‌اى"],
+
+
+
 ];
 
-const additionalReplacementRule = (str) => {
+const additionalReplacementRule = (str) => {    
     return str.split(" ")
         .map(n => 
           n.replace(/\u064A$/,"ی")) // "ي" -> "ی"
